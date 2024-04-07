932787a5b43291745c676630c16e4c9ba256f0d1
cowannb17 <cowannb17@uww.edu>
2024-04-07 02:51:36 -0500

    Fixed syntax so program can run

--- server.py
+++ server.py
@@ -15,8 +15,10 @@ def accept_connection(sock):
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopot(socket.SOL_SOCKET, socket.SO_REUSERADDR, 1)
-sock.bind(('0.0.0.0', 12756))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+print("Opening Server at 127.0.0.1 on port 12756")
+sock.bind(('127.0.0.1', 12756))
 sock.listen()
 
 accept_connection(sock)
