9aa02918606fcf3bf65c18e8d7ecc93d0158b926
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-11 16:05:39 -0500

    Added paramaters.

--- user.py
+++ user.py
@@ -1,11 +1,14 @@
 class user:
-    def __init__(self):
+    def __init__(self, userID, userIP):
         self.userID = None
         self.userIP = None
         self.requestedFiles = []
         self.downloads = []
 
-    def requestFiles(self):
-        self.requestedFiles = []
+    def requestFiles(self, files):
+        self.requestedFiles = [] # A list of files that the user wants to download
+
+    def fileDownload(self, file):
+        self.downloads.append(file)
 
 
