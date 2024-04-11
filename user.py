b458b3ccd7cc07562965688a9fccaf45470a33c7
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-11 15:55:56 -0500

    Added user class.

---
+++ user.py
@@ -0,0 +1,11 @@
+class user:
+    def __init__(self):
+        self.userID = None
+        self.userIP = None
+        self.requestedFiles = []
+        self.downloads = []
+
+    def requestFiles(self):
+        self.requestedFiles = []
+
+
