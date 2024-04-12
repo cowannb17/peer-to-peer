b8bd544279706aae0eef00db0a35bfbc28b55fe0
Nelson Cowan <cowannb17@uww.edu>
2024-04-12 10:48:09 -0500

    Added comments, segmented connection to server
    Added methods for encoding and decoding the incoming connection data.
    Added methods for saving and getting UUID from keyring.
    Added new method "connect to server" which helps with adding connecting to the server multiple times.
    Added ability for downloads fetch to open a new socket connection to the server.
    Changed initialize connection method to connect to server then request the downloads

--- server.py
+++ server.py
@@ -8,15 +8,22 @@ def accept_connection(sock):
     with conn:
         conn.sendall(b'secure_code')
         uuid_data = conn.recv(1024)
+        
         if not uuid_data:
             conn.close()
             return
+        
         if b'UUID: ' not in uuid_data and b'first time user' not in uuid_data:
             conn.close()
             return
+        
         if b'first time user' in uuid_data:
             print("First time user")
             # Grant UUID to connector and create user
+        
+        if b'UUID: ' in uuid_data:
+            conn.send(b'verified')
+
         while True:
             data = conn.recv(1024)
             if not data:
