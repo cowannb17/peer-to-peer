484936a313807a47018aa10f0c1ae71b3bddf54e
PhantomMist <gamerboy7853@gmail.com>
2024-04-27 11:48:44 -0500

    Added three functions for the data in database.py

--- database.py
+++ database.py
@@ -10,6 +10,27 @@ class database:
         try:
             cursor.execute(f"CREATE TABLE {tableName} ({columns})")
             conn.commit()
-        except:
+        except :
             print("Table already exists")
     
+    def insert_data(self, tableName, data):
+        try:
+            cursor.execute(f"insert into {tableName} values ({data})")
+            conn.commit()
+        except Exception:
+            print("error inserting data:" )
+    
+    def select_data(self, tableName, columns):
+        try:
+            cursor.execute(f"select {columns} from {tableName}")
+            return cursor.fetchall() 
+        except Exception:
+            print("error selecting data:" )
+    
+    def update_data(self, tableName, data):
+        try:
+            cursor.execute(f"update {tableName} set {data}")
+            conn.commit()
+        except Exception:
+            print("error updating data:" )
+            
