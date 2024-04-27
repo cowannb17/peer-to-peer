df9ce2f4e37124d586077653dcb7c9b029c4d09f
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-27 10:41:48 -0500

    Added RSA for key sharing.

--- encryption.py
+++ encryption.py
@@ -1,8 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 class encryption:
     def __init__(self):
@@ -59,3 +58,37 @@ class encryption:
 
     def get_nonce(self):
         return self.nonce
+
+    def export_key(self, recipient_public_key):
+        """
+        Encrypts the AES key with the recipient's public RSA key.
+
+        Args:
+            recipient_public_key (str): The recipient's public RSA key.
+
+        Returns:
+            bytes: The encrypted AES key.
+
+        """
+        rsa_public_key = RSA.import_key(recipient_public_key)
+        cipher_rsa = PKCS1_OAEP.new(rsa_public_key)
+        encrypted_key = cipher_rsa.encrypt(self.key)
+        return encrypted_key
+
+    def import_key(self, encrypted_key, recipient_private_key):
+        """
+        Imports and decrypts an AES key using the recipient's private RSA key.
+
+        Args:
+            encrypted_key (bytes): The encrypted AES key.
+            recipient_private_key (str): The recipient's private RSA key.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If the recipient's private key is invalid or the decryption fails.
+        """
+        rsa_private_key = RSA.import_key(recipient_private_key)
+        cipher_rsa = PKCS1_OAEP.new(rsa_private_key)
+        self.key = cipher_rsa.decrypt(encrypted_key)
