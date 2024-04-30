cd98b804ce676382db3f5a208e74bdfe6d5b3ab7
Joey Maffiola <7maffiolajoey@gmail.com>
2024-04-30 12:52:44 -0500

    Added compatability for messages > 53 bytes.

--- messageMethods.py
+++ messageMethods.py
@@ -3,8 +3,19 @@ import socket
 
 def sendRsa(message: str, publicKey: rsa.PublicKey, socket: socket.socket):
     encodedMessage = message.encode('utf-8')
-    encryptedMessage = rsa.encrypt(encodedMessage, publicKey)
-    socket.sendall(encryptedMessage)
+    if len(encodedMessage) <= 53: # Message is short enough to send in one go
+        encryptedMessage = rsa.encrypt(encodedMessage, publicKey)
+        socket.sendall(encryptedMessage)
+        return
+
+    # Message is too long, split it into chunks
+    for i in range(0, len(encodedMessage), 53):
+        chunk = encodedMessage[i:i+53]
+        encryptedMessage = rsa.encrypt(chunk, publicKey)
+        socket.sendall(encryptedMessage)
+    
+    sendRsa("file_end", publicKey, socket) # Send a message to indicate the end of the file
+
 
 def recieveRsa(privateKey: rsa.PrivateKey, socket: socket.socket):
     encryptedMessage = socket.recv(1024)
