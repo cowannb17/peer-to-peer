18e0ed6b2f221512dce171e3617011986355d75f
Joey Maffiola <7maffiolajoey@gmail.com>
2024-05-01 14:57:02 -0500

    Refactored delKeys.py.

--- delKeys.py
+++ delKeys.py
@@ -1,15 +1,12 @@
 import keyring
 
 # print keyring
-pubkey = keyring.get_password("p2p", "pubkey")
-privkey = keyring.get_password("p2p", "privkey")
-server_pubkey = keyring.get_password("p2p", "server_pubkey")
-uuid = keyring.get_password("p2p", "uuid")
 
-print("pubkey: ", pubkey)
-print("privkey: ", privkey)
-print("server_pubkey: ", server_pubkey)
-print("uuid: ", uuid)
+print("pubkey: ", keyring.get_password("p2p", "pubkey"))
+print("privkey: ", keyring.get_password("p2p", "privkey"))
+print("server_pubkey: ", keyring.get_password("p2p", "server_pubkey"))
+print("uuid: ", keyring.get_password("p2p", "uuid"))
+
 
 def delete_password(service, username):
     if keyring.get_password(service, username) is not None:
@@ -20,7 +17,7 @@ delete_password("p2p", "privkey")
 delete_password("p2p", "server_pubkey")
 delete_password("p2p", "uuid")
 
-print("pubkey: ", pubkey)
-print("privkey: ", privkey)
-print("server_pubkey: ", server_pubkey)
-print("uuid: ", uuid)
+print("pubkey: ", keyring.get_password("p2p", "pubkey"))
+print("privkey: ", keyring.get_password("p2p", "privkey"))
+print("server_pubkey: ", keyring.get_password("p2p", "server_pubkey"))
+print("uuid: ", keyring.get_password("p2p", "uuid"))
